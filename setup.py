98ec558e16dac823177dd37ebbf94f8c9b02b773
DARKora7 <morgtdicks@gmail.com>
2019-04-16 13:09:08 +0200

    altered setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/DARKora7/httpRequests.git",
-    packages=setuptools.find_packages(),
+    packages=['httpRequest','used_fuctions'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
