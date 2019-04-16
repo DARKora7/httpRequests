87ea86c7a02cb2f37d4c239575f8a5e5e7a63ab6
DARKora7 <morgtdicks@gmail.com>
2019-04-16 12:40:44 +0200

    created init and setup files

---
+++ httpRequest/httpRequest.py
@@ -0,0 +1,21 @@
+import requests
+from requests.exceptions import HTTPError
+from used_functions import *
+
+
+
+try:
+    url = "https://jsonplaceholder.typicode.com/users"
+    response = requests.get(url)
+    response.raise_for_status
+except HTTPError as http_er:
+    print(f'Http Error has occured: {http_er}')
+except Exception as er:
+    print(f'An Error has occured: {er}')
+finally:
+    print("connected")
+    if validateData(response.json()):
+        SortedCustList = ExtractList(response.json())
+        MakeTextFile(SortedCustList)
+    else:
+        quit()
