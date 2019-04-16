2d591000137752a47bd241efb34137dd0c4a04f2
DARKora7 <morgtdicks@gmail.com>
2019-04-16 12:49:54 +0200

    setup.py completed

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="httpRequest",
+    version="0.0.1",
+    author="DARKora7",
+    author_email="morgtdicks@gmail.com",
+    description="will extract json data and write to textfile",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DARKora7/httpRequests.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
