50b00c4b4f5a25c2f605ee2472055edacafc1376
DARKora7 <morgtdicks@gmail.com>
2019-04-16 13:20:18 +0200

    Finding and fixing bugs

--- httpRequest/httpRequest.py
+++ httpRequest/httpRequest.py
@@ -2,22 +2,21 @@ import requests
 from requests.exceptions import HTTPError
 from used_functions import *
 
-if __name__ =="__main__":
-    pass
 
 
-try:
-    url = "https://jsonplaceholder.typicode.com/users"
-    response = requests.get(url)
-    response.raise_for_status
-except HTTPError as http_er:
-    print(f'Http Error has occured: {http_er}')
-except Exception as er:
-    print(f'An Error has occured: {er}')
-finally:
-    print("connected")
-    if validateData(response.json()):
-        SortedCustList = ExtractList(response.json())
-        MakeTextFile(SortedCustList)
-    else:
-        quit()
+def run():
+    try:
+        url = "https://jsonplaceholder.typicode.com/users"
+        response = requests.get(url)
+        response.raise_for_status
+    except HTTPError as http_er:
+        print(f'Http Error has occured: {http_er}')
+    except Exception as er:
+        print(f'An Error has occured: {er}')
+    finally:
+        print("connected")
+        if validateData(response.json()):
+            SortedCustList = ExtractList(response.json())
+            MakeTextFile(SortedCustList)
+        else:
+            quit()
